046a5ea8215b6f36bd69a6445904ffe709550c4d
vip-weizhen <47912513+vip-weizhen@users.noreply.github.com>
2021-07-16 19:08:30 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,61 @@
 const html = `<!DOCTYPE html>
 <body>
 
+央视频道,#genre#
+CCTV1综合,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226226/1.m3u8<br>
+CCTV2财经,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226230/1.m3u8<br>
+CCTV3综艺,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226471/1.m3u8<br>
+CCTV4国际,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226335/1.m3u8<br>
+CCTV5体育,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226469/1.m3u8<br>
+CCTV5+赛事,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226225/1.m3u8<br>
+CCTV6电影,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226468/1.m3u8<br>
+CCTV7军事,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226234/1.m3u8<br>
+CCTV8视剧,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226485/1.m3u8<br>
+CCTV9纪录,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226236/1.m3u8<br>
+CCTV10科教,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226227/1.m3u8<br>
+CCTV11戏曲,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226334/1.m3u8<br>
+CCTV12社会,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226228/1.m3u8<br>
+CCTV13新闻,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226316/1.m3u8<br>
+CCTV14少儿,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226229/1.m3u8<br>
+CCTV15音乐,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226460/1.m3u8<br>
+CCTV17农业,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226318/1.m3u8<br>
 
-CCTV-1 综合 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225642/2/index.m3u8?fmt=ts2hls<br>
-CCTV-2 财经 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225643/2/index.m3u8?fmt=ts2hls<br>
-CCTV-3 综艺 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225634/2/index.m3u8?fmt=ts2hls<br>
-CCTV-5 体育 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225633/2/index.m3u8?fmt=ts2hls<br>
-CCTV-5+ 体育赛事 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225706/2/index.m3u8?fmt=ts2hls<br>
-CCTV-6 电影 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225632/2/index.m3u8?fmt=ts2hls<br>
-CCTV-7 国防军事 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225644/2/index.m3u8?fmt=ts2hls<br>
-CCTV-8 电视剧 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225631/2/index.m3u8?fmt=ts2hls<br>
-CCTV-9 纪录 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225646/2/index.m3u8?fmt=ts2hls<br>
-CCTV-10 科教 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225636/2/index.m3u8?fmt=ts2hls<br>
-CCTV-12 社会与法 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225637/2/index.m3u8?fmt=ts2hls<br>
-CCTV-14 少儿 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225640/2/index.m3u8?fmt=ts2hls<br>
-CCTV-17 农业农村 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225909/2/index.m3u8?fmt=ts2hls<br>
-中央新影 老故事,http://39.135.138.5:6610/PLTV/88888888/224/3221225517/2/index.m3u8<br>
-中央新影 中学生,http://39.135.138.5:6610/PLTV/88888888/224/3221225515/2/index.m3u8<br>
-中央新影 新科动漫,http://cctvtxyh5ca.liveplay.myqcloud.com/live/xinkedongman_2/index.m3u8<br>
-中央新影 发现之旅,http://125.210.152.18:9090/live/FXZL_750.m3u8<br>
-CETV-1 高清,http://39.135.138.8:6610/PLTV/88888888/224/3221225917/2/index.m3u8?fmt=ts2hls<br>
-CETV 早期教育,http://39.135.138.8:6610/PLTV/88888888/224/3221225513/2/index.m3u8?fmt=ts2hls<br>
-中国气象,http://hls.weathertv.cn/tslslive/qCFIfHB/hls/live_sd.m3u8<br>
+各省卫视,#genre#
+北京卫视,http://101.71.255.229:6610/zjhs/2/10136/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+上海卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226237/1.m3u8<br>
+重庆卫视,http://39.135.53.195/ott.fj.chinamobile.com/PLTV/88888888/224/3221225949/1.m3u8<br>
+天津卫视,http://101.71.255.229:6610/zjhs/2/10150/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+新疆卫视,http://livehyw5.chinamcache.com/hyw/zb01.m3u8<br>
+浙江卫视,http://39.135.53.195/ott.fj.chinamobile.com/PLTV/88888888/224/3221225934/1.m3u8
+福建卫视,rtsp://183.252.176.54:554/PLTV/88888888/224/3221226121/10000100000000060000000002358085_0.smil<br>
+海峡卫视,rtsp://183.252.176.54:554/PLTV/88888888/224/3221226128/10000100000000060000000002434539_0.smil<br>
+厦门卫视,http://39.135.53.195/ott.fj.chinamobile.com/PLTV/88888888/224/3221226781/1.m3u8<br>
+湖南卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226509/1.m3u8<br>
+江苏卫视,rtsp://183.252.176.54/PLTV/88888888/224/3221226038/10000100000000060000000002296879_0.smil<br>
+江西卫视,http://101.71.255.229:6610/zjhs/2/10145/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+青海卫视,http://live.geermurmt.com/qhws/sd/live.m3u8<br>
+云南卫视,http://39.134.115.163:8080/PLTV/88888910/224/3221225664/index.m3u8<br>
+贵州卫视,rtsp://183.252.166.199:554/PLTV/88888888/224/3221226126/10000100000000060000000002434541_0.smil<br>
+四川卫视,http://101.71.255.229:6610/zjhs/2/10144/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+安徽卫视,http://101.71.255.229:6610/zjhs/2/10141/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+山东卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226244/1.m3u8<br>
+甘肃卫视,http://39.134.39.38/PLTV/88888888/224/3221226240/index.m3u8<br>
+广东卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226238/1.m3u8<br>
+广西卫视,http://39.134.115.163:8080/PLTV/88888910/224/3221225731/index.m3u8<br>
+深圳卫视,http://39.135.53.195/ott.fj.chinamobile.com/PLTV/88888888/224/3221225848/1.m3u8<br>
+康巴卫视,http://scgctvshow.sctv.com/hdlive/kangba/1.m3u8<br>
+湖北卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226240/1.m3u8<br>
+河北卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226507/1.m3u8<br>
+河南卫视,http://101.71.255.229:6610/zjhs/2/10153/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+海南卫视,rtsp://183.252.166.199:554/PLTV/88888888/224/3221226048/10000100000000060000000002296889_0.smil<br>
+宁夏卫视,https://hhytv.ningxiahuangheyun.com/live/nxws1M.m3u8<br>
+山西卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226531/index.m3u8<br>
+陕西卫视,http://39.134.115.163:8080/PLTV/88888910/224/3221225729/index.m3u8<br>
+三沙卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226544/index.m3u8<br>
+辽宁卫视,http://101.71.255.229:6610/zjhs/2/10148/index.m3u8?virtualDomain=zjhs.live_hls.zte.com<br>
+吉林卫视,http://39.134.115.163:8080/PLTV/88888910/224/3221225680/index.m3u8<br>
+黑龙江卫视,http://otttv.bj.chinamobile.com/TVOD/88888888/224/3221226239/1.m3u8<br>
+内蒙古卫视,http://live.m2oplus.nmtv.cn/1/sd/live.m3u8<br>
 
 
 </body>`
